5858bf117c45959a8736219161c09d5130ada438
unknown <m.giri1309@gmail.com>
2024-07-22 22:35:26 +0700

    fixing conn.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT;
 
 db.sync()
   .then(() => {
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server is running on port ${PORT}`);
     });
   })
