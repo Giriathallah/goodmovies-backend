46c409c7e6232c8ff466fae588fc2256a12d82dc
unknown <m.giri1309@gmail.com>
2024-07-23 21:07:37 +0700

    fixing vercel

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: "https://goodmovies-two.vercel.app", // Allow all origins
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: "*",
 };
 
 app.use(cors(corsOptions));
