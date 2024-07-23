a8f17278d6c672827f87f34c337c79780b0f0836
unknown <m.giri1309@gmail.com>
2024-07-23 08:18:17 +0700

    fixing vercel

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
-const corsOptions = {
-  origin: "http://localhost:5173", // your frontend's origin
-  optionsSuccessStatus: 200, // For legacy browser support
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: "http://localhost:5173", // your frontend's origin
+//   optionsSuccessStatus: 200, // For legacy browser support
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+// };
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
